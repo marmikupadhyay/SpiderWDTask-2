208ddf0b1bc8d64e8beb8fb33bccd49105a86a87
Marmik Upadhyay <marmikupadhyay.mu@gmail.com>
2020-05-26 23:48:09 +0530

    Commit-12:Added Delete and added functionality to read more button.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,16 +5,19 @@ const Post = require("../models/Post");
 
 const { ensureAuthenticated } = require("../config/auth");
 
+//Get Request to Main Panel
 router.get("/panel", ensureAuthenticated, (req, res) => {
-  Post.find({})
+  Post.find({}) //Finding all the posts
     .sort("-date")
     .then(allPosts => {
+      //Finding posts to the specific user
       Post.find({ author: req.user._id })
+        .sort("-date")
         .then(userPosts => {
           res.render("userpanel", {
             user: req.user,
-            allPosts,
-            userPosts
+            allPosts, //sending all posts
+            userPosts //sending user specific posts
           });
         })
         .catch(err => {
@@ -26,6 +29,7 @@ router.get("/panel", ensureAuthenticated, (req, res) => {
     });
 });
 
+//Handleing Post addition
 router.post("/add", ensureAuthenticated, (req, res) => {
   const { title, body } = req.body;
   const newPost = new Post({
@@ -42,6 +46,18 @@ router.post("/add", ensureAuthenticated, (req, res) => {
     })
     .catch(err => {
       console.log(err);
+    });
+});
+
+//Handling Post Deletion
+router.get("/delete:id", ensureAuthenticated, (req, res) => {
+  Post.findOneAndDelete({ _id: req.params.id })
+    .then(post => {
+      req.flash("success_msg", "Post Deleted");
+      res.redirect("/user/panel");
+    })
+    .catch(err => {
+      err;
     });
 });
 
